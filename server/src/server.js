7b0dc0a9abad664f735451c5ce0025f07e8b7719
Arun Hariharan <arunhariharan21@gmail.com>
2023-09-05 17:50:31 -0400

    Created Initial Mongoose Schemas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http'); 
+const mongoose = require('mongoose'); 
 
 const app = require('./app');
 
@@ -6,9 +7,20 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://ahari21:ahf4CiRvZ6WLRrEe@cluster0.2r3cxex.mongodb.net/?retryWrites=true&w=majority"; 
+
 const server = http.createServer(app); 
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL); 
     await loadPlanetsData();
 
     server.listen(PORT, ()=>{
